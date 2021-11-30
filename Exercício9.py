707e88f697112903f26d19278e7c1093019a18ca
felipe <felipesfischer@gmail.com>
2021-11-29 21:33:53 -0300

    exercícios em python

--- Exercício9.py
+++ Exercício9.py
@@ -1,1 +1,17 @@
 # 9 - Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+import random
+from typing import List
+
+numeros = []
+
+for lista in range(0,10):
+    numero_aleatorio = random.randrange(0,100)
+    numeros.append(numero_aleatorio)
+
+print(numeros)
+
+numeros.reverse()
+
+print(numeros)
+
---
+++ Exercícios10.py
@@ -0,0 +1,13 @@
+# 10 - Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+notas = [7, 8, 9, 10]
+soma_das_notas = 0
+
+for nota in notas:
+    soma_das_notas += nota
+    media = (soma_das_notas / 4)
+print(media)
+    
+# print(soma_das_notas)
+    
+  
