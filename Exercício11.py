5bc546c7bbd640c81ab6c6ee639db0023fc1fc3c
felipe <felipesfischer@gmail.com>
2021-12-11 12:53:19 -0300

    exercícios em python

--- Exercício11.py
+++ Exercício11.py
@@ -1,2 +1,16 @@
 # 11 - Faça um Programa que leia um vetor de 10 caracteres, 
 # e diga quantas consoantes foram lidas. Imprima as consoantes.
+
+vogais = ["a","e","i,","o","u"]
+consoantes = []
+i = 10
+
+    
+for x in consoantes:
+    letras = input("Digite uma letra: ")
+    if letras != vogais:
+        consoantes.append(letras)
+    else:
+        print("Você não digitou nenhuma consoante")
+    i -= 1
+print(consoantes)
